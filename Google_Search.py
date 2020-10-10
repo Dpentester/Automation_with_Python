431b0e50f79466baf756c0a3151e8f6c857209cd
dillu345 <dileepdil345@gmail.com>
2020-10-10 13:25:53 -0400

    Added Youtbe_Search_Automation.py

--- Google_Search.py
+++ Google_Search.py
@@ -1,9 +1,6 @@
 # import webdriver
 from selenium import webdriver
-
-# import time
 import time
-
 # open the browser
 driver = webdriver.Chrome()
 
@@ -12,14 +9,14 @@ driver.get("https://www.google.com")
 
 # searchbox
 searchbox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-searchbox.clear()
 searchbox.send_keys('alphahalf')
+
 
 # searchbutton
 searchbutton = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[3]/center/input[1]')
 searchbutton.click()
 
-time.sleep(7)
+time.sleep(3)
 
 # close the browser
 driver.close()
